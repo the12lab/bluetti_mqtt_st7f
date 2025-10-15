e5311ceab8f23765174e755aab2ea3d43e667408
the12lab <the12lab@gmail.com>
2025-10-15 17:44:32 -0500

    Update __init__.py

--- bluetti_mqtt/bluetooth/__init__.py
+++ bluetti_mqtt/bluetooth/__init__.py
@@ -26,7 +26,7 @@ async def scan_devices():
 
 def build_device(address: str, name: str):
     print(f'BD {name}: {address}')
-    match = DEVICE_NAME_RE.match('EB3A1234')#(name)
+    match = DEVICE_NAME_RE.match(name)#('EB3A1234')#(name)
     if not match:
         raise Exception("device not supported (does not match device name regexp)")
     if match[1] == 'AC200M':
